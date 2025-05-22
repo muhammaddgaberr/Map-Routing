d0bc684c61dd797067cb2c5c7469ecbeb1269656
Mohamed Gaber <137436028+muhammaddgaberr@users.noreply.github.com>
2025-05-22 06:01:47 +0300

    Update Speed_Bonus.h

--- MAP ROUTING/Speed_Bonus.h
+++ MAP ROUTING/Speed_Bonus.h
@@ -14,24 +14,25 @@ class Speed_Bonus
 {
 
     vector<int> added_to_destination;
+    vector<pair<int, int>> has_been_deleted;
 
 public :
     void fixing_Adj_List_bonus(
         vector<vector<Read_file::Adj_nodes_bonus>>& adj_list,
-        vector<pair<float, float>>& co_ordinates,
-        float source_x, float source_y,
-        float destination_x, float destination_y, float R);
-    
-    void Billman_ford_bonus(
-        vector<vector<Read_file::Adj_nodes_bonus>>& adj_list,
+        vector<pair<double, double>>& co_ordinates,
+        double source_x, double source_y,
+        double destination_x, double destination_y, double R, int co_ordinates_size);
+    void Dijkstra_edited(
+        vector<vector<Read_file::Adj_nodes_bonus>> &adj_list,
         int source, int destination,
-        vector<int>& path, float& shortest_time,
-        float& path_length, float& total_walking_distance,
-        float& total_vehicle_distance,
-        float speed_interval, int speed_count);
+        vector<int>& path, double& shortest_time,
+        double& path_length, double& total_walking_distance,
+        double& total_vehicle_distance,
+        double speed_interval_hours,
+        int speed_count);
+
+    void orig_list(vector<pair<int, int>>& has_been_deleted);
 
     void Original_Adj_list_bonus(vector<vector<Read_file::Adj_nodes_bonus>>& adj_list);
-
-
 };
 
