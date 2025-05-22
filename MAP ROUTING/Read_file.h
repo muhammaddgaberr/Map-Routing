6c10b50270e89eef45e71490c6647876e9799c6a
Mohamed Gaber <137436028+muhammaddgaberr@users.noreply.github.com>
2025-05-22 06:01:29 +0300

    Update Read_file.h

--- MAP ROUTING/Read_file.h
+++ MAP ROUTING/Read_file.h
@@ -12,29 +12,29 @@ class Read_file
 
 public:
 	struct Query {
-		float X_source, Y_source, X_destination, Y_destination, R;
+		double X_source, Y_source, X_destination, Y_destination, R;
 	};
 
 	struct Adj_nodes {
 		int node;
-		float distance;
-		float time;
+		double distance;
+		double time;
 	};
 
 	struct Adj_nodes_bonus
 	{
 		int node;
-		float distance;
-		vector<float> time;
+		double distance;
+		vector<double> time;
 	};
 
 	// normal cases
-	void read_map(string file_name, vector<pair<float, float>>& co_ordinates, vector<vector<Read_file::Adj_nodes>>& adj_list);
+	void read_map(string file_name, vector<pair<double, double>>& co_ordinates,
+				  vector<vector<Read_file::Adj_nodes>>& adj_list, int &N_nodes);
 	void read_queries(string file_name, vector<Query>& queries,int &N_queries);
-
 	// bonus cases
-	void read_map_bonus(string file_name, vector<pair<float, float>>& co_ordinates, vector<vector<Read_file::Adj_nodes_bonus>>& adj_list,
-		float &speed_interval,int &speed_count);
+	void read_map_bonus(string file_name, vector<pair<double, double>>& co_ordinates, vector<vector<Read_file::Adj_nodes_bonus>>& adj_list,
+		double&speed_interval,int &speed_count,int &N_nodes);
 
 };
 
